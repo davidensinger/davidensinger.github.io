1a3b10828ce8d21d79720e72cb82cb470f10f74d
David Ensinger <david.ensinger@gmail.com>
2013-04-14 11:10:44 -0400

    Added the Jekyll Tagging plugin.

---
+++ _plugins/ext.rb
@@ -0,0 +1,8 @@
+require 'jekyll/tagging'
+
+module Jekyll
+  # Makes the blog tag urls "pretty" instead of /tag.html its just /tag/
+  # This allows me to keep a custom permalink setting for blog posts
+  Filters::PRETTY_URL = true
+  class TagPage; def template; '/:basename/' end end
+end
