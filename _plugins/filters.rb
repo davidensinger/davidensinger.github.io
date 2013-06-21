c0d2474c62c6744380725b4841940c247a26e3b1
David Ensinger <hello@davidensinger.com>
2013-06-21 16:52:13 -0400

    Added a plugin filter for a proper titlecase now that Jekyll down cases post category names.

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -5,6 +5,10 @@ module MyFilters
 			$1+url+$3
 		end
 	end
-end
 
+  # Returns a title cased string based on John Gruber's title case http://daringfireball.net/2008/08/title_case_update
+   def titlecase(input)
+     input.titlecase
+   end
+end
 Liquid::Template.register_filter MyFilters
