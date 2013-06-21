c7e74657ba726cc64a0b3767767b473da4d660d9
David Ensinger <hello@davidensinger.com>
2013-06-21 16:56:30 -0400

    Removed ext.rb because I don't believe I need it for the pretty permalinks.

--- _plugins/ext.rb
+++
@@ -1,6 +0,0 @@
-module Jekyll
-  # Makes the blog tag urls "pretty" instead of /tag.html its just /tag/
-  # This allows me to keep a custom permalink setting for blog posts
-  Filters::PRETTY_URL = true
-  class TagPage; def template; '/:basename/' end end
-end
