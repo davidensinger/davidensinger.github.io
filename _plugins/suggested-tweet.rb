04cdddaec983e2759585dca79b5cecfc00ec7938
David Ensinger <hello@davidensinger.com>
2013-09-03 12:18:14 -0400

    Update documentation for Suggested Tweet plugin.

--- _plugins/suggested-tweet.rb
+++ _plugins/suggested-tweet.rb
@@ -1,25 +1,43 @@
-# A Liquid tag for Jekyll that allows for the embedding of suggested tweets.
+# Suggested Tweet Tag
+#
+# A Liquid tag for Jekyll that allows for the embedding of suggested tweets,
+# via Twitter’s Web Intents API and @richhollis’s Twitter Web Intents Ruby Gem.
 #
 # Author: David Ensinger
-# Source URL: Forthcoming
+# Site: http://davidensinger.com
+# Twitter: @DavidEnsinger
+# Email: hello@davidensinger.com
+# Source: Forthcoming
 #
-# Example usage:
+# A big thanks to Rich Hollis’s Twitter Web Intents Ruby Gem upon which this plugin is based.
+# Source: https://github.com/richhollis/twitter_web_intents
+#
+# Configuration:
+# The following parameters may be set globally in _config.yml or on a per page basis in the YAML front-matter.
+# All are optional. Parameters set in the YAML front-matter take precedence over those set in _config.yml.
+#
+#   suggested_tweet:
+#     url:                 'http://davidensinger.com/'
+#     via:                 'davidensinger'
+#     text:                'Hello world'
+#     in_reply_to:         331434728957833218
+#     hashtags:            ['Jekyll', 'Twitter']
+#     related:             ['davidensinger', 'richhollis']
+#     inline_inheritance:  false
+#
+# Default usage:
 #   {% suggested_tweet %}
 #
-# Documentation:
-#   Forthcoming
+# Inline configuration:
+# The tag also accepts inline parameters. The inheritance of global variables (inline_inheritance) is set to false by default.
 #
-
-# Functionality
-# • Only Tweet or Reply to a Tweet (see Intents docs)
-# • Appends supported parameters (url, via, text, in_reply_to, hashtags, and related)
-# • Global variables for supported parameters are found in _config.yml, but per post variables may be defined in post's YAML front matter.
-# • JavaScript is optional
-# • Count characters to stay under 140 character limit? Not sure how.
+# Inline usage:
+#   {% suggested_tweet url:'http://davidensinger.com/' via:'davidensinger' text:'Hello world' in_reply_to:331434728957833218 hashtags:['Jekyll', 'Twitter'] related:['davidensinger', 'richhollis'] %}
+#
 
 # https://dev.twitter.com/docs/intents
 # http://jekyllrb.com/docs/plugins/#tags
-# http://jekyllrb.com/docs/templates/#filters
+# https://github.com/Shopify/liquid/wiki/Liquid-for-Designers
 # https://github.com/ericdfields/Jekyll-Dribbble-Set-Tag/blob/master/_plugins/dribbble_set.rb (for _config.yml stuff)
 # https://github.com/richhollis/twitter_web_intents
 
