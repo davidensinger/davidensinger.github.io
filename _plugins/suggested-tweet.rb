e9ad182be913e3e6f1bd8e6237b80ee4b71d3e26
David Ensinger <hello@davidensinger.com>
2013-09-04 10:56:21 -0400

    Improve the documentation for the Suggested Tweet tag to hopefully make it more clear.

--- _plugins/suggested-tweet.rb
+++ _plugins/suggested-tweet.rb
@@ -12,28 +12,39 @@
 # A big thanks to Rich Hollis’s Twitter Web Intents Ruby Gem upon which this plugin is based.
 # Source: https://github.com/richhollis/twitter_web_intents
 #
-# Configuration:
+# Default Configuration:
 # The following parameters may be set globally in _config.yml or on a per page basis in the YAML front-matter.
-# All are optional. Parameters set in the YAML front-matter take precedence over those set in _config.yml.
+# Parameters set in the YAML front-matter take precedence over those set in _config.yml. Note that all parameters
+# are optional. By default, the inheritance of global parameters (default_inheritance in _config.yml) is set to true.
 #
 #   suggested_tweet:
-#     url:                 'http://davidensinger.com/'
-#     via:                 'davidensinger'
-#     text:                'Hello world'
-#     in_reply_to:         331434728957833218
-#     hashtags:            ['Jekyll', 'Twitter']
-#     related:             ['davidensinger', 'richhollis']
-#     inline_inheritance:  false
+#     url:                  'http://davidensinger.com/'
+#     via:                  'davidensinger'
+#     text:                 'Hello world'
+#     in_reply_to:          331434728957833218
+#     hashtags:             ['Jekyll', 'Twitter']
+#     related:              ['davidensinger', 'richhollis']
+#     container_tag:        'span'
+#     container_class:      'tweet-container'
+#     anchor_class:         'tweet-link'
+#     default_inheritance:  true
+#     inline_inheritance:   false
 #
-# Default usage:
+# Default Usage:
 #   {% suggested_tweet %}
 #
-# Inline configuration:
-# The tag also accepts inline parameters. The inheritance of global variables (inline_inheritance) is set to false by default.
+# Default Output:
+#   <span class="tweet-container"><a href="https://twitter.com/intent/tweet?hashtags=Jekyll,Twitter&in_reply_to=331434728957833218&related=davidensinger,richhollis&text=Hello+world&url=http%3A%2F%2Fdavidensinger.com&via=davidensinger" class="suggested-tweet">Hello world</a></span>
 #
-# Inline usage:
-#   {% suggested_tweet url:'http://davidensinger.com/' via:'davidensinger' text:'Hello world' in_reply_to:331434728957833218 hashtags:['Jekyll', 'Twitter'] related:['davidensinger', 'richhollis'] %}
+# Inline Configuration:
+# The tag also accepts inline parameters. By default, the inheritance of global parameters (inline_inheritance in _config.yml) is set to false.
+# Note that tags that include parameters inline inherit nothing from parameters set in the YAML front-matter of the page.
 #
+# Inline Usage:
+#   {% suggested_tweet container_tag:'span' container_class:'tweet-container' anchor_class:'tweet-link' url:'http://davidensinger.com/' via:'davidensinger' text:'Hello world' in_reply_to:331434728957833218 hashtags:['Jekyll', 'Twitter'] related:['davidensinger', 'richhollis'] %}
+#
+# Inline Output:
+#   <span class="tweet-container"><a href="https://twitter.com/intent/tweet?hashtags=Jekyll,Twitter&in_reply_to=331434728957833218&related=davidensinger,richhollis&text=Hello+world&url=http%3A%2F%2Fdavidensinger.com&via=davidensinger" class="suggested-tweet">Hello world</a></span>
 
 # https://dev.twitter.com/docs/intents
 # http://jekyllrb.com/docs/plugins/#tags
