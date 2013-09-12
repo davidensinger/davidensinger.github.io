3b2db808ce34c8763f16ceab802f7b905c26c47d
David Ensinger <hello@davidensinger.com>
2013-09-12 11:47:58 -0400

    Update suggested-tweet.rb plugin with John’s information.

--- _plugins/suggested-tweet.rb
+++ _plugins/suggested-tweet.rb
@@ -3,16 +3,21 @@
 # A Liquid tag for Jekyll that allows for the embedding of suggested tweets,
 # via Twitter’s Web Intents API and @richhollis’s Twitter Web Intents Ruby Gem.
 #
-# Author: David Ensinger
-# Site: http://davidensinger.com
-# Twitter: @DavidEnsinger
-# Email: hello@davidensinger.com
+# Authors: David Ensinger and John Colvin
+# Site: http://davidensinger.com/ and http://2john4tv.biz/
+# Twitter: @DavidEnsinger and @2john4tv
+# Email: hello@davidensinger.com and 2john4tv@gmail.com
 # Source: Forthcoming
+#
+# Really John did all the Ruby work. I just bought him pizza and told him what I wanted :)
 #
 # A big thanks to Rich Hollis’s Twitter Web Intents Ruby Gem upon which this plugin is based.
 # Source: https://github.com/richhollis/twitter_web_intents
 #
-# Default Configuration:
+# Please note that you’ll need to install the Twitter Web Intents gem as it’s required.
+#
+# Configuration:
+#
 # The following parameters may be set globally in _config.yml or on a per page basis in the YAML front-matter.
 # Parameters set in the YAML front-matter take precedence over those set in _config.yml. Note that all parameters
 # are optional.
@@ -23,13 +28,19 @@
 #     text:                 'Hello world'
 #     in_reply_to:          331434728957833218
 #     hashtags:             ['Jekyll', 'Twitter']
-#     related:              ['davidensinger', 'richhollis']
+#     related:              ['davidensinger', 'richhollis', '2john4tv']
 #
-# Default Usage:
+# Usage:
 #   {% suggested_tweet %}
 #
-# Default Output:
-#   https://twitter.com/intent/tweet?hashtags=Jekyll,Twitter&in_reply_to=331434728957833218&related=davidensinger,richhollis&text=Hello+world&url=http%3A%2F%2Fdavidensinger.com&via=davidensinger
+# Output:
+#   https://twitter.com/intent/tweet?hashtags=Jekyll,Twitter&in_reply_to=331434728957833218&related=davidensinger,richhollis,2john4tv&text=Hello+world&url=http%3A%2F%2Fdavidensinger.com&via=davidensinger
+#
+# Liquid Output for Parameters (using text as an example):
+#    _config.yml: {{ site.suggested_tweet.text }}
+#    YAML front matter: {{ page.suggested_tweet.text }}
+#
+# Todo: If hashtags and related are both set in _config.yml and YAML front matter then combine arrays
 
 require 'twitter_web_intents'
 
