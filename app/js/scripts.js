af9db386874ec9f53c0f0c6d633d45fe01dd953a
David Ensinger <hello@davidensinger.com>
2015-01-11 16:31:40 -0500

    Remove DOMContentLoaded event listener from keyboard navigation JS in scripts.js

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,26 +1,23 @@
-document.addEventListener('DOMContentLoaded', function(){
-
-  // Keyboard navigation
-  function keyboardNavigation(ID) {
-    var element = document.getElementById('js-navigation--' + ID);
-    if (element) {
-      var anchor = element.getAttribute('href');
-      window.location.href = anchor;
-      return false;
-    }
+// Keyboard navigation
+function keyboardNavigation(ID) {
+  var element = document.getElementById('js-navigation--' + ID);
+  if (element) {
+    var anchor = element.getAttribute('href');
+    window.location.href = anchor;
+    return false;
   }
+}
 
-  document.onkeydown = function(e) {
-    e = e || window.event;
-    switch(e.which || e.keyCode) {
-      case 37: // left
-        keyboardNavigation('previous');
-        break;
-      case 39: // right
-        keyboardNavigation('next');
-        break;
-      default: return;
-    }
-    e.preventDefault();
-  };
-});
+document.onkeydown = function(e) {
+  e = e || window.event;
+  switch(e.which || e.keyCode) {
+    case 37: // left
+      keyboardNavigation('previous');
+      break;
+    case 39: // right
+      keyboardNavigation('next');
+      break;
+    default: return;
+  }
+  e.preventDefault();
+};
